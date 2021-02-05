42e1d868cb45872832a057ae14f2f4cb5df6dab6
unknown <mdaley115@gmail.com>
2021-02-05 15:42:01 -0500

    added copy to clipboard

--- src/translator/output.js
+++ src/translator/output.js
@@ -1,7 +1,40 @@
+import { useState, useRef, useEffect } from 'react'
+
 export default function Output({ translation }) {
+    const outputText = useRef(null)
+    const [copied, setCopied] = useState(false)
+
+    function copyToClip() {
+        outputText.current.select()
+        document.execCommand("copy")
+    }
+
+    function copiedToClip() {
+        setCopied(true)
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCopied(false)
+        }, 2000)
+    }, [copied])
+
 return (
     <div className="output-container">
         <h2>Ⱦŗå∩ѕ⌠åŁëd</h2>
-        <textarea className="output" readOnly={true} spellCheck={false} value={translation}/>
+        
+        {copied &&  
+            <div className="copied-message">Copied!</div>
+        }
+        <textarea
+            ref={outputText}
+            className="output"
+            onClick={copyToClip}
+            readOnly={true}
+            spellCheck={false}
+            value={translation}
+            onCopy={copiedToClip}
+            />
+        <p>Click the text to copy to clipboard</p>
     </div>
 )}
